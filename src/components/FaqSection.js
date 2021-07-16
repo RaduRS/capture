18d3cb4530726746635a0c56b98c0e813657162f
RaduRS <rsrusu90@gmail.com>
2021-07-16 11:59:57 +0100

    Faq Section

---
+++ src/components/FaqSection.js
@@ -0,0 +1,68 @@
+import React from "react";
+
+const FaqSection = () => {
+  return (
+    <div className="faq">
+      <h2>
+        Any Question <span>FAQ</span>
+      </h2>
+      <div className="question">
+        <h4>How Do I Start?</h4>
+        <div className="answer">
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae
+            accusamus.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet nihil
+            maiores perferendis excepturi saepe deleniti consectetur.
+          </p>
+        </div>
+      </div>
+
+      <div className="question">
+        <h4>Daily Schedule</h4>
+        <div className="answer">
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae
+            accusamus.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet nihil
+            maiores perferendis excepturi saepe deleniti consectetur.
+          </p>
+        </div>
+      </div>
+
+      <div className="question">
+        <h4>Different Payment Methods</h4>
+        <div className="answer">
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae
+            accusamus.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet nihil
+            maiores perferendis excepturi saepe deleniti consectetur.
+          </p>
+        </div>
+      </div>
+
+      <div className="question">
+        <h4>What Products Do We Offer?</h4>
+        <div className="answer">
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae
+            accusamus.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet nihil
+            maiores perferendis excepturi saepe deleniti consectetur.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FaqSection;
